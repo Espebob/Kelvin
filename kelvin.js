b5842f75b3913913c67e6e98f43de3545d1c5576
Espebob <84918128+Espebob@users.noreply.github.com>
2024-02-20 10:52:54 +0100

    uploaded a file
    temperature conveter

---
+++ kelvin.js
@@ -0,0 +1,19 @@
+const kelvin = 0;
+//assigning constant variable kelvin to 293
+
+const celsius = kelvin - 273;
+//assigning another constant variable to kelvin minus 273
+
+let fahrenheit = celsius * (9/5) + 32;
+//another variable declaration and initiation
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+// converting Celsius to Newton
+
+let newton = celsius * (33/100);
+newtnon = Math.floor(newton);//rounding
+
+console.log(`The temperature is ${newton} degrees Newton`);
+//displaying my name and my age in dog years
+
